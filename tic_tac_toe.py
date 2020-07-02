86047cae8e089b0ede6445ebce96941fd86ac634
Zaharakatd <37711267+Zaharakatd22@users.noreply.github.com>
2020-07-02 10:25:06 +0400

    Realize funtional of printing current game status from input 9 symbols

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,7 +1,20 @@
 def main():
-    print("X O X\n"
-          "O X X\n"
-          "O X O")
+    cells = input("Enter cells: > ")
+    print("---------")
+    curr_row = 1
+    curr_symbol = 0
+    for elem in cells:
+        curr_symbol += 1
+        if curr_symbol % 3 == 0:
+            print(elem + " ", end="")
+            print("|")
+            curr_row += 1
+        elif (curr_symbol - 1) % 3 == 0:
+            print("| ", end="")
+        if curr_symbol % 3 > 0:
+            print(elem + " ", end="")
+
+    print("---------")
 
 
 if __name__ == "__main__":
