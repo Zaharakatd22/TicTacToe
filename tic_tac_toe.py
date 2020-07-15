345658210e5ab9fd060841e946fe958f7bb626be
Zaharakatd <37711267+Zaharakatd22@users.noreply.github.com>
2020-07-15 21:22:23 +0400

    Refactor code on matrix curr_state view and realize working game loop.

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,11 +1,13 @@
 class TicTacToe:
-    def __init__(self, curr_state: str):
-        self.curr_state: list = [[curr_state[i + j] for j in range(6, -1, -3)] for i in range(0, 3)]
+    def __init__(self):
+        self.curr_state: list = [["_", "_", "_"] for _ in range(0, 3)]
         self.win_count: int = 0
         self.winner: str = ""
         self.is_possible: bool = True
         self.is_draw: bool = True
         self.game_status: bool = False
+        self.curr_symbol: str = "X"
+        self.curr_step: int = 0
 
     def __repr__(self):
         return "Console game +-+-+-+TicTacToe+-+-+-+: \n" \
@@ -14,7 +16,8 @@ class TicTacToe:
                f"Winner: {self.winner} \n" \
                f"Is possible: {self.is_possible} \n" \
                f"Is draw: {self.is_draw} \n" \
-               f"Game status is {int(self.game_status)}"
+               f"Game status is {int(self.game_status)} \n" \
+               f"Current symbol is {self.curr_symbol}"
 
     def __str__(self):
         return f"TicTacToe(curr_state: {self.curr_state}, " \
@@ -22,33 +25,34 @@ class TicTacToe:
                f"winner: {self.winner}, " \
                f"is_possible: {self.is_possible}, " \
                f"is_draw: {self.is_draw}, " \
-               f"game_status: {self.game_status})"
+               f"game_status: {self.game_status}, " \
+               f"curr_symbol: {self.curr_symbol})"
 
     def check_horizontal_win(self):
-        for i in range(0, 8, 3):
-            if self.curr_state[i:i+3] == "XXX":
+        for i in range(0, 3):
+            if "".join(self.curr_state[i]) == "XXX":
                 self.winner = "X"
                 self.win_count += 1
-            elif self.curr_state[i:i+3] == "OOO":
+            elif "".join(self.curr_state[i]) == "OOO":
                 self.winner = "O"
                 self.win_count += 1
 
     def check_vertical_win(self):
         for i in range(0, 3):
-            if self.curr_state[i] + self.curr_state[i + 3] + self.curr_state[i + 6] == "XXX":
+            if self.curr_state[0][i] + self.curr_state[1][i] + self.curr_state[2][i] == "XXX":
                 self.winner = "X"
                 self.win_count += 1
-            elif self.curr_state[i] + self.curr_state[i + 3] + self.curr_state[i + 6] == "OOO":
+            elif self.curr_state[0][i] + self.curr_state[1][i] + self.curr_state[2][i] == "OOO":
                 self.winner = "O"
                 self.win_count += 1
 
     def check_dioganally_win(self):
-        if self.curr_state[0] + self.curr_state[2] + self.curr_state[8] == "XXX" \
-                or self.curr_state[2] + self.curr_state[4] + self.curr_state[6] == "XXX":
+        if self.curr_state[0][0] + self.curr_state[1][1] + self.curr_state[2][2] == "XXX" \
+                or self.curr_state[0][2] + self.curr_state[1][1] + self.curr_state[2][0] == "XXX":
             self.winner = "X"
             self.win_count += 1
-        elif self.curr_state[0] + self.curr_state[2] + self.curr_state[8] == "OOO" \
-                or self.curr_state[2] + self.curr_state[4] + self.curr_state[6] == "OOO":
+        elif self.curr_state[0][0] + self.curr_state[1][1] + self.curr_state[2][2] == "OOO" \
+                or self.curr_state[0][2] + self.curr_state[1][1] + self.curr_state[2][0] == "OOO":
             self.winner = "O"
             self.win_count += 1
 
@@ -57,16 +61,8 @@ class TicTacToe:
         self.check_vertical_win()
         self.check_dioganally_win()
 
-    def check_possible(self):
-        null_count = self.curr_state.count("O")
-        x_count = self.curr_state.count("X")
-
-        self.is_possible = self.win_count <= 1 and (null_count == x_count - 1 or x_count == null_count - 1
-                                                    or null_count == x_count)
-
     def check_draw(self):
-        empty_count = self.curr_state.count("_")
-        self.is_draw = empty_count == 0
+        self.is_draw = self.curr_step == 9 and self.win_count == 0
 
     @staticmethod
     def print_border():
@@ -94,6 +90,7 @@ class TicTacToe:
 
     def player_move(self):
         correct_inp: bool = False
+        self.curr_step += 1
         while not correct_inp:
             coord: list = input("Enter the coordinates: > ").split()
             if len(coord) < 2 or not (coord[0].isdigit() and coord[1].isdigit()):
@@ -103,38 +100,34 @@ class TicTacToe:
             elif self.curr_state[int(coord[0]) - 1][int(coord[1]) - 1] != "_":
                 print("This cell is occupied! Choose another one!")
             else:  # always is correct
-                self.curr_state[int(coord[0]) - 1][int(coord[1]) - 1] = "X"
+                self.curr_state[int(coord[0]) - 1][int(coord[1]) - 1] = self.curr_symbol
                 correct_inp = True
 
     def start(self):
-        print(self)
         self.game_status = True
         self.print_curr_state()
-        self.player_move()
-        self.print_curr_state()
-        # self.check_win()
-        # self.check_possible()
-        # self.check_draw()
-        # if self.is_possible:
-        #     if self.is_draw and self.win_count == 0:
-        #         print("Draw")
-        #         self.game_status = False
-        #     elif not self.is_draw and self.win_count == 0:
-        #         print("Game not finished")
-        #     else:
-        #         print(f"{self.winner} wins")
-        #         self.game_status = False
-        # else:
-        #     print("Impossible")
-        #     self.game_status = False
+        while self.game_status:
+            self.player_move()
+            self.print_curr_state()
+            self.check_win()
+            self.check_draw()
+            if self.is_draw:
+                print("Draw")
+                self.game_status = False
+            elif self.win_count == 1:
+                print(f"{self.winner} wins")
+                self.game_status = False
+
+            if self.curr_symbol == "X":
+                self.curr_symbol = "O"
+            else:
+                self.curr_symbol = "X"
 
 
 def main():
-    cells = input("Enter cells: > ")
-    tic_tac_toe = TicTacToe(cells)
+    tic_tac_toe = TicTacToe()
     tic_tac_toe.start()
 
 
 if __name__ == "__main__":
     main()
-
